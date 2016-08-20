7a0f1cf7a16c04cf94f9c5b821f018b6133ec00a
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-08-20 07:30:11 -0400

    add working test

--- textminer/validator.py
+++ textminer/validator.py
@@ -31,15 +31,16 @@ def phone_number(pn):
 
 
 def money(mo):
-    return re.search(r'^\$?-?0*(?:\d+(?!,)(?:\.\d{1,2})?|(?:\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?))$', mo)
-
+    #return re.match(r'^\$([0-9]{1,3})(,?[0-9]{3})*(\.[0-9){2})?$', mo)
+    return re.match(r'^\$[0-9]{1,3}(?:,?[0-9{3})*(?:\.[0-9]{2})?', mo)
 
 def zipcode(zp):
     return re.search(r"^(^\d{5}-\d{4})|(^\d{5})+$", zp)
     # return re.search(r'^(^\d{5}-\d{4})|(^\d{5})+$', string)
 
 def date(gametime):
-    return( [r"(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<year>\d{4}|\d{2})",
-                  r"(?P<year>\d{4})-?(?P<month>\d{2})-?(?P<day>\d{2})",
-                  r"(?P<day>\d{1,2})\s*(?P<month>[A-Za-z]{3})\s*(?P<year>\d{4})",
-                  r"(?P<month>[A-Za-z]{3})\s*(?P<day>\d{1,2})\s*,?\s*(?P<year>\d{4})"], gametime)
+    # return( [r"(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<year>\d{4}|\d{2})",
+    #               r"(?P<year>\d{4})-?(?P<month>\d{2})-?(?P<day>\d{2})",
+    #               r"(?P<day>\d{1,2})\s*(?P<month>[A-Za-z]{3})\s*(?P<year>\d{4})",
+    #               r"(?P<month>[A-Za-z]{3})\s*(?P<day>\d{1,2})\s*,?\s*(?P<year>\d{4})"], gametime)
+    return re.match(r'[0-9]{1,4}[\/-][0-9]{1,2}[\/-][0-9]{2,4}', gametime)
